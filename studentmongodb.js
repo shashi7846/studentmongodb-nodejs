b0ca4dc4093f3d8b13f9533432b234a585482fb1
shashi7846 <shashikanth7846@gmail.com>
2021-04-14 18:11:31 +0530

    db added

--- studentmongodb.js
+++ studentmongodb.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongodb = require("mongodb");
-const URL = "mongodb+srv://shashikanth:user123@cluster0.pviae.mongodb.net?retryWrites=true&w=majority";
-const DB = "studentmongodb.js";
-// const bcrypt=require("bcryptjs")
+const URL =   "mongodb+srv://shashikanth:user123@cluster0.pviae.mongodb.net?retryWrites=true&w=majority";
+const DB = "studentmongodb";
+const bcrypt = require("bcryptjs");
 app.use(cors());
 app.use(express.json());
 
@@ -20,52 +20,104 @@ app.get("/students", async function (req, res) {
     res.json(students);
   } catch (error) {
     console.log(erroe);
-  }   
+  }
 });
 
-// app.post("/register", async function(req,res){
-//     try {
-//         let connection =await mongodb.connect(URL);
-//         let db = connection.db(DB);
-//         //Genereate a salt
-//         let salt = await bcrypt.genSalt(10)
-//         //Hash the password with salt
-//         let hash = await bcrypt.hash(req.body.password,salt)
-//         //store it in DB
-//         req.body.password=hash;
-//         let users = await db.collection("users").insertOne(res.body);
-//         await connection.close()
+app.post("/register", async function (req, res) {
+  try {
+    let connection = await mongodb.connect(URL);
+    let db = connection.db(DB);
+    let isEmailUnique = await db.collection("users").findOne({email:req.body.email});
+    if(isEmailUnique){
+      res.status(401).json({
+        message:"Email already exists"
+      })
+    }else{
+    //Genereate a salt
+    let salt = await bcrypt.genSalt(10);
+    //Hash the password with salt
+    let hash = await bcrypt.hash(req.body.password, salt);
+
+    //store it in DB
+    req.body.password = hash;
+
+    let users = await db.collection("users").insertOne(req.body);
+    await connection.close();
+    res.json({
+      message: "User Registerd",
+    })
+  }
+  } catch (error) {}
+});
+
+// app.post("/login", async function (req, res) {
+
+//   try {
+
+//     let connection = await mongodb.connect(URL);
+
+//     let db = connection.db(DB);
+
+//     //Find the user with email
+
+//     let user = await db.collection("users").findOne({ email: req.body.email })
+
+//     //hash the password
+
+//     if (user) {
+
+//       let isPassword = await bcrypt.compare(res.body.password , user.password)
+
+//       if (isPassword) {
+
 //         res.json({
-//             message:"user registered"
+
+//           message: "Allow"
 //         })
-//     } catch (error) {
-//         console.log(error)
+//       } else {
+//         res.status(404).json({
+//           message: "email or password is incorrect"
+//         })
+//       }
+//     } else {
+
+//       res.status(404).json({
+
+//         message: "Email or Password is InCorrect"
+//       })
 //     }
+//   } catch (error) {
+//     console.log(error)
+//   }
 // })
-   
-// app.post("/login",async function(req,res){
-//     try {
-//         let connection = await mongodb.connect(URL)
-//         let db = connection.db(DB);
-//         //Find the user with email
-//         let user = await db.collection("users").findOne({email: req.body})
-//         //hash the password
 
-//         if(user){
-//             let isPasswordCorrect = await bcrypt.compare(res.body.password,user.password)
+app.post("/login",async function(req,res){
+  try {
+      let connection = await mongodb.connect(URL);
+      let db = connection.db(DB);
 
-//         }
-        
-//         else{
-//             res.status(404).json({
-//                 message:"Email or Password is InCorrect"
-//             })        }
+      let user=await db.collection("users").findOne({email : req.body.email})
 
-
-//     } catch (error) {
-        
-//     }
-// })
+      if(user){
+          let isPassword=await bcrypt.compare(req.body.password,user.password);
+          if(isPassword){
+              res.json({
+                  message:"allow"
+              })
+          }else{
+              res.status(404).json({
+                  message:"Email or password is incorrect"
+              })
+          }
+      }else{
+          res.status(404).json({
+              message:"Email or password is incorrect"
+          })
+      }
+  } catch (error) {
+      
+  }
+})
 
 app.post("/student", async function (req, res) {
   // req.body.id = students.length + 1;
@@ -123,17 +175,25 @@ app.put("/student/:id", async function (req, res) {
 });
 
 app.delete("/student/:id", async function (req, res) {
-try {
-    let connection =await  mongodb.connect(URL);
+  try {
+    let connection = await mongodb.connect(URL);
     let db = connection.db(DB);
-    await db.collection("students").deleteOne({_id: mongodb.ObjectID(req.params.id)})
-    await connection.close()
+    await db
+      .collection("students")
+      .deleteOne({ _id: mongodb.ObjectID(req.params.id) });
+    await connection.close();
     res.json({
-        message:"deleted"
-    })
-} catch (error) {
-    console.log(error)
-}
+      message: "deleted",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+app.get("/dashboard",function(req,res){
+  res.json({
+    message:"Secure data"
+  })
+})
 
 app.listen(process.env.PORT || 3000);
